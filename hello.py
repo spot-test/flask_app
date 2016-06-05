cc92e6e381a4dea0b2334d00269b456c9e3fc77b
spot-test <cableshaun@gmail.com>
2016-06-05 14:32:28 -0700

    Hello World Flask app
    Initial checkin

---
+++ hello.py
@@ -0,0 +1,18 @@
+#!C:\Python27\python
+
+###################################################################
+#
+#   This is a "Hello World!" Python script
+#
+#
+#
+###################################################################
+
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
